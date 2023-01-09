19e1d8c6b9211410add80f7371ffe42683c50a13
kauan777 <kauan7622@gmail.com>
2023-01-09 10:42:00 -0300

    feat: adding new state and useEffect

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,21 +1,40 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
 export function Event() {
+  const [isSideBarVisible, setIsSideBarVisible] = useState<boolean>(false);
+  const { slug } = useParams<{ slug: string }>();
 
-    const { slug } =  useParams<{slug: string}>();
+  function handleResize() {
+    if (window.innerWidth > 768) {
+      setIsSideBarVisible(false);
+    }
+  }
 
-    return (
-        <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex flex-1">
-                { slug 
-                ? <Video lessonSlug={slug}/> 
-                : <div className="flex-1"/> }
-                <Sidebar />
-            </main>
-        </div>
-    )
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header
+        isSideBarVisible={isSideBarVisible}
+        setIsSideBarVisible={setIsSideBarVisible}
+      />
+      <main className="flex flex-1">
+        {slug ? (
+          <Video lessonSlug={slug} isSideBarVisible={isSideBarVisible} />
+        ) : (
+          <div className="flex-1" />
+        )}
+        <Sidebar isOpen={isSideBarVisible} />
+      </main>
+    </div>
+  );
 }
