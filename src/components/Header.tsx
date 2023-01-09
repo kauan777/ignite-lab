3b7e0c4ebb7fabdf77aecbd3e1d21649064ae3d8
kauan777 <kauan7622@gmail.com>
2023-01-09 10:40:58 -0300

    feat: adding condition and new styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,35 @@
+import { List, X } from "phosphor-react";
 import { Logo } from "./Logo";
 
-// Tailwind: padding padrão (p) = 4px   py-5 = 20  'cause 5 * 4 = 20
+// Tailwind: padding defaults (p) = 4px   py-5 = 20  'cause 5 * 4 = 20
 
-export function Header() {
-    return (
-        <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
-            <Logo />
-        </header>
-    )
+interface HeaderProps {
+  isSideBarVisible: boolean;
+  setIsSideBarVisible(isSideBarVisible: boolean): void;
+}
+
+export function Header({ isSideBarVisible, setIsSideBarVisible }: HeaderProps) {
+  function openSideBar() {
+    setIsSideBarVisible(true)
+  }
+
+  function closeSideBar() {
+    setIsSideBarVisible(false)
+
+  }
+
+  return (
+    <header className="w-full py-5 px-8 flex justify-between items-center md:justify-center bg-gray-700 border-b border-gray-600">
+      <Logo />
+      {isSideBarVisible ? (
+        <button className="block md:hidden" onClick={closeSideBar}>
+          <X size={32} color="#cbd5e1" />
+        </button>
+      ) : (
+        <button className="block md:hidden" onClick={openSideBar}>
+          <List size={32} color="#cbd5e1" />
+        </button>
+      )}
+    </header>
+  );
 }
